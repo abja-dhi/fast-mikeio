e99c9d3fa4566c8210ec2946110cb8b34f798b65
abja-dhi <abja@dhigroup.com>
2025-10-22 10:21:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Installation
+Clone the repo
+python setup.py sdist bdist_wheel
+pip install .
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fast-mikeio",
+    version="0.0.1",
+    author="Abbas Jazaeri",
+    author_email="abja@dhigroup.com",
+    description="A faster version of mikeio package for reading DHI MIKE output files.", 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abja-dhi/fast-mikeio",
+    packages=['fast_mikeio'],
+    install_requires=[
+        'numpy',
+        'mikecore',
+        'matplotlib',
+        'scipy',
+        'cycler',
+        'tqdm',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
